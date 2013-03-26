b617ffed25b1163063afe7a41f5bc56ff5cfa545
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-03-26 15:29:52 -0400

    Fix promotion rules attr_accessible. [Fix #12]

--- app/models/spree/promotion/rules/product_decorator.rb
+++ app/models/spree/promotion/rules/product_decorator.rb
@@ -3,6 +3,7 @@ module Spree
     module Rules
       Product.class_eval do
         belongs_to :product_group
+        attr_accessible :products_source, :product_group_id
 
         def eligible_products
           product_group ? product_group.products : products
