8c844dc6a6aa0b4477264d5349cf09c1ac4d4336
Zubin Henner <zubin.henner@gmail.com>
2013-05-28 10:56:00 +1000

    Prevent "superclass mismatch for class Promotion" error.
    * was happening intermittently in development
    * there were no other gems with `Promotion' class
    * was unable to replicate error in tests

--- app/models/spree/promotion/rules/product_decorator.rb
+++ app/models/spree/promotion/rules/product_decorator.rb
@@ -1,20 +1,22 @@
-module Spree
-  class Promotion < Spree::Activator
-    module Rules
-      Product.class_eval do
-        belongs_to :product_group
-        attr_accessible :products_source, :product_group_id
+unless defined?(Spree::Promotion)
+  module Spree
+    class Promotion < Spree::Activator
+      module Rules
+        Product.class_eval do
+          belongs_to :product_group
+          attr_accessible :products_source, :product_group_id
 
-        def eligible_products
-          product_group ? product_group.products : products
-        end
-
-        def products_source=(source)
-          if source.to_s == 'manual'
-            self.product_group_id = nil
+          def eligible_products
+            product_group ? product_group.products : products
           end
-        end
 
+          def products_source=(source)
+            if source.to_s == 'manual'
+              self.product_group_id = nil
+            end
+          end
+
+        end
       end
     end
   end
