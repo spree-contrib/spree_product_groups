937098de2aa0065a4eea97e695ca6b8e8ae36314
Ryan Bigg <radarlistener@gmail.com>
2013-09-26 16:22:02 +1000

    Use get in routes for Rails 4 compat

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Spree::Core::Engine.routes.draw do
 
   # Search routes
-  match 's/*product_group_query', :to => 'products#index', :as => :simple_search
-  match '/pg/:product_group_name', :to => 'products#index', :as => :pg_search
-  match '/t/*id/s/*product_group_query', :to => 'taxons#show', :as => :taxons_search
-  match 't/*id/pg/:product_group_name', :to => 'taxons#show', :as => :taxons_pg_search
+  get 's/*product_group_query', :to => 'products#index', :as => :simple_search
+  get '/pg/:product_group_name', :to => 'products#index', :as => :pg_search
+  get '/t/*id/s/*product_group_query', :to => 'taxons#show', :as => :taxons_search
+  get 't/*id/pg/:product_group_name', :to => 'taxons#show', :as => :taxons_pg_search
 
   namespace :admin do
     resources :product_groups do
